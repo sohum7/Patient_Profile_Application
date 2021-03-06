8c3865a1ec687ce9d87889d1b2a730cc3cf0d821
sohum7 <57050073+sohum7@users.noreply.github.com>
2021-03-06 14:01:35 -0500

    Update PatientProfDBInterface.java

--- PatientProfDBInterface.java
+++ PatientProfDBInterface.java
@@ -1,6 +1,5 @@
-//test
-
-
+////  Patient Profile Interface for admin users
+//      Interface will allow admin users (doctors, nurses, etc) to access profile data
 public abstract PatientProfDBInterfaceAbstract(){
   
 }
