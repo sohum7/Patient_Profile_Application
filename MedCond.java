a185e9748d1fed9bff16c990ff4ffacd278147af
Jkocian-t <kocianj00@gmail.com>
2021-03-18 18:27:15 -0400

    Added errors for incorrect inputs for some variables

--- MedCond.java
+++ MedCond.java
@@ -2,21 +2,43 @@
 
 public class MedCond{
     // arguments  vvvvv
-    String mdContact;
-    String mdPhone;
-    String algType;
-    String illType;
+    private String mdContact;
+    private String mdPhone;
+    private String algType;
+    private String illType;
     // arguments ^^^^^
-    
+
     //constructor vvvv
-    public MedCond(){
-        mdContact = "String";
-        mdPhone = "String";
-        algType = "String";
-        illType = "String";
-    }
+    public MedCond(String MDcontact, String MDphone, String Algtype, String Illtype){
+        mdContact = MDcontact;
+        mdPhone = MDphone;
+        int t = 1;
+        try{
+            if(Algtype == "none" || Algtype == "food" || Algtype == "medication" || Algtype == "other"){
+                algType = Algtype;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"none\", \"food\", \"medication\", or \"other\", SET TO null.");
+            }
+
+
+
+        try{
+            if(Illtype == "none" || Illtype == "CHD" || Illtype == "diabetes" || Illtype == "asthma" || Illtype == "other"){
+                illType = Illtype;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"none\", \"CHD\", \"diabetes\", \"asthma\", or \"other\", SET TO null.");
+            }
+
+        }
+
     //constructor ^^^^^^^
-    
+
     //get methods, they print the info required, and return them vvvvvvvvvvvvvv
     public String getmdContact(){
         System.out.println(mdContact);
@@ -35,7 +57,7 @@ public class MedCond{
         return illType;
     }
     //get methods, they print the info required, and return them ^^^^^^^^^^^^^
-    
+
     //update methods, they update their respective arguments vvvvvvvvvvvvvvvvv
     public void updatemdContact(String n){
         mdContact = n;
@@ -44,22 +66,44 @@ public class MedCond{
         mdPhone = n;
     }
     public void updateAlgType(String n){
-        algType = n;
+        int t = 1;
+        try{
+            if(n == "none" || n == "food" || n == "medication" || n == "other"){
+                algType = n;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"none\", \"food\", \"medication\", or \"other\", NOT UPDATED.");
+            }
+
+
     }
     public void updateIllType(String n){
-        illType = n;
+        int t = 1;
+        try{
+            if(n == "none" || n == "CHD" || n == "diabetes" || n == "asthma" || n == "other"){
+                illType = n;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"none\", \"CHD\", \"diabetes\", \"asthma\", or \"other\", NOT UPDATED.");
+            }
+
+
     }
     //update methods, they update their respective arguments  ^^^^^^^^^^^^^^^^^^^^^
-    
-    
+
+
     //test main function
     /*public static void main(String[] args){
-        MedCond test = new MedCond();
+        MedCond test = new MedCond("Dr.Winslow", "8884445555", "none", "none");
         test.getAlgType();
         test.getmdContact();
         test.getmdPhone();
         test.getIllType();
-        test.updateAlgType("Penniciline");
+        test.updateAlgType("medication");
         test.updateIllType("None");
         test.updatemdContact("Dr.Doctorson");
         test.updatemdPhone("5558763333");
--- PatientProf.java
+++ PatientProf.java
@@ -7,106 +7,151 @@
 
 import java.io.Serializable; public class PatientProf implements Serializable{
     // arguments vvvvvvvvvvvv
-    String adminID;
-    String firstName;
-    String lastName;
-    String address;
-    String phone;
-    float coPay;
-    String insuType;
-    String patientType;
-    MedCond medCondInfo;
+    private String adminID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+    private float coPay;
+    private String insuType;
+    private String patientType;
+    private MedCond medCondInfo;
     //arguments ^^^^^^^^^^^^^^^^
-    
+
     //Constructor vvvvvvvvvvvvv
-    public PatientProf() {
-        adminID = "String";
-        firstName = "String";
-        lastName = "String";
-        address = "String";
-        phone = "String";
-        coPay = 0;
-        insuType = "String";
-        patientType = "String";
-        medCondInfo = new MedCond();
+    public PatientProf(String Adminid, String Firstname, String Lastname, String Address, String Phone, float Copay, String Insutype, String Patienttype, MedCond mdcnd) {
+        adminID = Adminid;
+        firstName = Firstname;
+        lastName = Lastname;
+        address = Address;
+        phone = Phone;
+        coPay = Copay;
+        int t = 1;
+        try{
+            if(Insutype == "Private" || Insutype == "Government"){
+                insuType = Insutype;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Insurance Type must be either \"Private\" or \"Government\", SET TO null.");
+            }
+
+
+        try{
+            if(Patienttype == "Senior" || Patienttype == "Pediatric" || Patienttype == "Adult"){
+                patientType = Patienttype;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"Senior\", \"Pediatric\", or \"Adult\", SET TO null.");
+            }
+
+
+        medCondInfo = mdcnd;
     }
     //Constructor ^^^^^^^^^^^^^^^^^^^
-    
+
     //get methods, they print and return their respective values vvvvvvvvvvvvv
-        public String getadminID(){
-            System.out.println(adminID);
-            return adminID;
-        }
-        public String getFirstName(){
-            System.out.println(firstName);
-            return firstName;
-        }
-        public String getLastName(){
-            System.out.println(lastName);
-            return lastName;
-        }
-        public String getAddress(){
-            System.out.println(address);
-            return address;
-        }
-        public String getPhone(){
-            System.out.println(phone);
-            return phone;
-        }
-        public String getInsuType(){
-            System.out.println(insuType);
-            return insuType;
-        }
-        public String getPatientType(){
-            System.out.println(patientType);
-            return patientType;
-        }
-        public void getMedCondInfo(){
-            medCondInfo.getAlgType();
-            medCondInfo.getmdContact();
-            medCondInfo.getmdPhone();
-            medCondInfo.getIllType();
-        }
-        public float getCoPay(){
-            System.out.println(coPay);
-            return coPay;
-        }
+    public String getadminID(){
+        System.out.println(adminID);
+        return adminID;
+    }
+    public String getFirstName(){
+        System.out.println(firstName);
+        return firstName;
+    }
+    public String getLastName(){
+        System.out.println(lastName);
+        return lastName;
+    }
+    public String getAddress(){
+        System.out.println(address);
+        return address;
+    }
+    public String getPhone(){
+        System.out.println(phone);
+        return phone;
+    }
+    public String getInsuType(){
+        System.out.println(insuType);
+        return insuType;
+    }
+    public String getPatientType(){
+        System.out.println(patientType);
+        return patientType;
+    }
+    public void getMedCondInfo(){
+        medCondInfo.getAlgType();
+        medCondInfo.getmdContact();
+        medCondInfo.getmdPhone();
+        medCondInfo.getIllType();
+    }
+    public float getCoPay(){
+        System.out.println(coPay);
+        return coPay;
+    }
     //get methods ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    
+
     //update methods, they update their respective values to what is inputed into them vvvvvvvvvvvvvvvvvvvvvv
-        public void updateadminID(String n){
-            adminID = n;
+    public void updateadminID(String n){
+        adminID = n;
+    }
+    public void updateFirstName(String n){
+        firstName = n;
+    }
+    public void updateLastName(String n){
+        lastName = n;
+    }
+    public void updateAddress(String n){
+        address = n;
+    }
+    public void updatePhone(String n){
+        phone = n;
+    }
+    public void updateInsuType(String n){
+        int t = 1;
+        try{
+            if(n == "Private" || n == "Government"){
+                insuType = n;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Insurance Type must be either \"Private\" or \"Government\", NOT UPDATED.");
+            }
+
         }
-        public void updateFirstName(String n){
-            firstName = n;
-        }
-        public void updateLastName(String n){
-            lastName = n;
-        }
-        public void updateAddress(String n){
-            address = n;
-        }
-        public void updatePhone(String n){
-            phone = n;
-        }
-        public void updateInsuType(String n){
-            insuType = n;
-        }
-        public void updatePatientType(String n){
-            patientType = n;
-        }
-        public void updateMedCondInfo(MedCond n){
-            medCondInfo = n;
-        }
-        public void updateCoPay(float n){
-            coPay = n;
-        }
-        // update methods ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    
-    
-        //test main function:
+
+
+    public void updatePatientType(String n){
+        int t = 1;
+        try{
+            if(n == "Senior" || n == "Pediatric" || n == "Adult"){
+                patientType = n;
+            }
+            else{
+                t = t / 0;}
+            }catch(Exception e){
+                System.out.println("ERROR: Patient Type must be \"Senior\", \"Pediatric\", or \"Adult\", NOT UPDATED.");
+            }
+
+
+    }
+    public void updateMedCondInfo(MedCond n){
+        medCondInfo = n;
+    }
+    public void updateCoPay(float n){
+        coPay = n;
+    }
+    // update methods ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+    //test main function:
         /*public static void main(String[] args){
-            PatientProf test = new PatientProf();
+            MedCond innertest = new MedCond("Dr.Winslow", "8884445555", "none", "none");
+            PatientProf test = new PatientProf("tyl100", "Pablo", "Digman", "123 Four Street", "8881112223", 20, "Government", "This Guy", innertest);
             test.getAddress();
             test.getadminID();
             test.getMedCondInfo();
@@ -119,7 +164,7 @@ import java.io.Serializable; public class PatientProf implements Serializable{
 
             test.updateAddress("444 Location Drive");
             test.updateadminID("Admin Cordiroy");
-            MedCond anotherone = new MedCond();
+            MedCond anotherone = new MedCond("Dr.Peterson", "134567222", "other", "other");
             anotherone.updateAlgType("Penniciline");
             anotherone.updateIllType("None");
             anotherone.updatemdContact("Dr.Doctorson");
@@ -127,7 +172,7 @@ import java.io.Serializable; public class PatientProf implements Serializable{
             test.updateMedCondInfo(anotherone);
             test.updateFirstName("Billy");
             test.updateLastName("Bob");
-            test.updatePatientType("Paying");
+            test.updatePatientType("adult");
             test.updatePhone("1234567");
             test.updateInsuType("FULL");
             test.updateCoPay(500);
@@ -142,4 +187,4 @@ import java.io.Serializable; public class PatientProf implements Serializable{
             test.getInsuType();
             test.getCoPay();
         }*/
-    }
+}
